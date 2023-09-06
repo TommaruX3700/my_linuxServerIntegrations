910534ec7c62a4a240241a1714e5fab443e87e9a
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-09-06 16:07:46 +0200

    installed NODE.js, express() and index.json done

--- API/index.js
+++ API/index.js
@@ -1,3 +1,13 @@
-//main API .js program
+const express = require('express');
+const app = express();
 
-const hello = {"ciao": ciao}
+// ENDPOINTS:
+app.get('/tmx37-api/isAlive', (req, res) => {
+    res.json({ message: 'API is alive, you  entered ' + req });
+});
+
+// SERVER CONFIGURATION
+const port = process.env.port || 3000; //port can be modified accordingly to use
+app.listen(port, () => {
+    console.log('Server is running on port ${port}');
+});
