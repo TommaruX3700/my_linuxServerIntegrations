1ff3af03a6e08580c022c8162e682aa1983499a7
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-09-19 15:42:46 +0200

    iniziato sviluppo delle due app principali

---
+++ Apps/Applications/AdminPannel_WebApp/react_frontend/src/GeneralOps_App/GeneralOpsApp.js
@@ -0,0 +1,17 @@
+import logo from '../media/logo.svg';
+import './GeneralOpsApp.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          APP1
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- Apps/Applications/AdminPannel_WebApp/react_frontend/src/index.js
+++ Apps/Applications/AdminPannel_WebApp/react_frontend/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 //TODO: ADD react-draggable module to implement drag&drop features
+
 import './index.css';
-import App from './App';
+import GeneralOpsApp from './GeneralOps_App/GeneralOpsApp.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello, world!</h1>
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <h1>AdminPannel_WebApp</h1>
+    <section class="layout">
+    <div class="sidebar">1</div>
+    <div class="body"><GeneralOpsApp /></div>
+    </section>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
