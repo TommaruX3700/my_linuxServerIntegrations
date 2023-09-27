9b1ba6a1c41551054f779f6aff3f57e0c2662d0c
TommaruX3700 <tommasomaruzzo@gmail.com>
2023-09-27 17:49:44 +0200

    asd

--- Applications/AdminPannel_WebApp/react_frontend/src/SpecMonitor_App/SpecMonitor_App.js
+++ Applications/AdminPannel_WebApp/react_frontend/src/SpecMonitor_App/SpecMonitor_App.js
@@ -1,6 +1,7 @@
-import logo from '../media/logo.svg';
+
 import './SpecMonitor_App.css';
 
+import Header from './js-modules/renderHeader.js'
 import Temps from './js-modules/renderTemps.js'
 import Performance from './js-modules/renderPerformance.js'
 import Network from './js-modules/renderNetwork.js'
@@ -10,10 +11,7 @@ function SpecMonitorApp() {
   return (
     <div className="pannelGlassBackground SpecMonitorApp">
 
-      <section class= "monitor-header">
-        <img src={logo} class="monitor-header-icon testLogo" alt="logo"/>
-        <p class="monitor-header-text">Specs Monitor</p>
-      </section>
+      <Header/>
 
       <div class="divider"></div>
       
---
+++ Applications/AdminPannel_WebApp/react_frontend/src/SpecMonitor_App/js-modules/renderHeader.js
@@ -0,0 +1,18 @@
+/*
+    This is for rendering the header of the WebApp
+*/
+
+import logo from '../media/logo.svg';
+
+function render () {
+    return (
+        <div>
+            <section class= "monitor-header">
+                <img src={logo} class="monitor-header-icon testLogo" alt="logo"/>
+                <p class="monitor-header-text">Specs Monitor</p>
+            </section>
+        </div>
+    );
+}
+
+export default render
